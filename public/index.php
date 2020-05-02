48cee661b9e0c84c9569b9f39d133330367fb63e
Aleksandr Novikov <pwsdotru@gmail.com>
2020-05-02 20:35:19 +0300

    Fix code style

--- public/index.php
+++ public/index.php
@@ -1,7 +1,5 @@
 <?php
-require_once (dirname(__DIR__) . '/app/autoload.php');
+require_once(dirname(__DIR__) . '/app/autoload.php');
 
 $app = new \Rest\Application();
-
 $app->run($_SERVER["REQUEST_URI"]);
-
